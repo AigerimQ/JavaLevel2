e355e6296312aeeb46ea145a0e820d3f79712288
AigerimQ <55206421+AigerimQ@users.noreply.github.com>
2020-03-24 17:12:01 +0600

    Урок 6

---
+++ NetworkServer/src/ru/geekbrains/java2/server/NetworkServer.java
@@ -0,0 +1,37 @@
+package ru.geekbrains.java2.server;
+
+import ru.geekbrains.java2.server.client.ClientHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkServer {
+
+    private final int port;
+    private final List<ClientHandler> clients = new ArrayList<>();
+
+    public NetworkServer(int port) {
+        this.port = port;
+    }
+
+    public void start() {
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            System.out.println("Сервер был успешно запущен на порту " + port);
+            while (true){
+                System.out.println("Ожидание клиентского подключения...");
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Клиент подключился");
+                clients.add(new ClientHandler(this, clientSocket));
+            }
+        } catch (IOException e) {
+            System.out.println("Ошибка при работе сервера");
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
